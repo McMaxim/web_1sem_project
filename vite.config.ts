9ebfbeb745494e1278219d7bffff85656d76ce37
McMaxim <94379675+McMaxim@users.noreply.github.com>
2025-12-19 19:01:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,5 @@ export default defineConfig(({ mode }) => ({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // для GitHub Pages: https://mcmaxim.github.io/web_1sem_project/
   base: mode === 'production' ? '/web_1sem_project/' : '/',
 }));
